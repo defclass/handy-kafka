9d478adec31b267adcfda0dcdefdf558e702dd79
defclass <defadvice@gmail.com>
2016-10-28 17:02:00 +0800

    update version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject defclass/handy-kafka "0.1.3-SNAPSHOT"
+(defproject defclass/handy-kafka "0.1.3"
   :description "A Clojure library designed to support..."
   :url "https://github.com/your-github-name/handy-kafka"
   :license {:name "Eclipse Public License"
